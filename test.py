5d0b5ed4a202d29cf8b48e2498b285f226b3ea80
Wadacmiaudas <as4646478596@gmail.com>
2021-07-26 15:20:36 +0800

    GG

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-img = cv2.imread('20180807002350.jpg')
-plt.show()
+img1 = cv2.imread("20180807002350.jpg")
+print(type(img1))
+# 顯示圖片
+cv2.imshow('My Image', img1)
+
+# 按下任意鍵則關閉所有視窗
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ test2.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+img = cv2.imread("20180807002350.jpg")
+cv2.line(img,(0,0),(511,511),(255,0,0),5)
+cv2.rectangle(img,(384,0),(510,128),(0,255,0),-1)
+# 顯示圖片
+cv2.imshow('My Image', img)
+
+# 按下任意鍵則關閉所有視窗
+cv2.waitKey(0)
+cv2.destroyAllWindows()
