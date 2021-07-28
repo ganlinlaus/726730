4c547693a526f6faeb7f70533ea28d89a2e57699
Wadacmiaudas <as4646478596@gmail.com>
2021-07-28 11:01:52 +0800

    want to sleep

---
+++ test29.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+def nothing(x):
+    pass
+
+img = cv2.imread('002.jpg', 0)
+img = cv2.resize(img, (750, 400))
+cv2.namedWindow('image')
+cv2.createTrackbar('min','image',0,255,nothing)
+cv2.createTrackbar('max','image',0,255,nothing)
+cv2.imshow('image',img)
+blur = cv2.GaussianBlur(img, (3, 3), 0)
+gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+
+
+while(True):
+    min = cv2.getTrackbarPos('min','image')
+    max = cv2.getTrackbarPos('max','image')
+    edges = cv2.Canny(img, min, max)
+    cv2.imshow('canny',edges)
+    if cv2.waitKey(1) == 27:
+        break
+cv2.destroyAllWindows()
