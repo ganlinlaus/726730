ac2b1d44ba422b2281409b0349062d94de8c7405
Wadacmiaudas <as4646478596@gmail.com>
2021-07-28 13:46:57 +0800

    XD

---
+++ test32.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('test6.jpg')
+lower = cv2.pyrDown(img)
+lower1 = cv2.pyrDown(lower)
+higher = cv2.pyrUp(lower1)
+higher1 = cv2.pyrUp(higher)
+laplace = cv2.subtract(img, higher1)
+
+print(img.shape, higher1.shape)
+
+plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)), plt.title("img")
+plt.subplot(122), plt.imshow(cv2.cvtColor(laplace, cv2.COLOR_BGR2RGB)), plt.title("laplace")
+plt.show()
+
