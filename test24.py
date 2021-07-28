57bd91df4f32814c80b066603126484a8dc8d257
Wadacmiaudas <as4646478596@gmail.com>
2021-07-28 10:04:46 +0800

    practice

---
+++ test24.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+img = cv2.imread('001.jpg', 0)
+
+
+sobel = cv2.Sobel(img, cv2.CV_8U, 1, 1, ksize=5)
+#sobelx = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=5)
+sobely = cv2.Sobel(sobel, cv2.CV_8U, 0, 1, ksize=5)
+
+cv2.imshow("img", img)
+cv2.imshow("sobel", sobel)
+#cv2.imshow("sobelx", sobelx)
+cv2.imshow("sobely", sobely)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
