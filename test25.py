259684d79356aa7e23b3ccdf9c5074d4b7ab1659
Wadacmiaudas <as4646478596@gmail.com>
2021-07-28 11:02:20 +0800

    ZZZZZZZ

--- test25.py
+++ test25.py
@@ -9,6 +9,10 @@ img = cv2.imread('001.jpg', 0)
 sobel = cv2.Sobel(img, cv2.CV_8U, 1, 1, ksize=5)
 sobelx = cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize=5)
 sobely = cv2.Sobel(img, cv2.CV_8U, 0, 1, ksize=5)
+
+
+
+
 sobelxy = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
 cv2.imshow("img", img)
 cv2.imshow("sobel", sobel)
